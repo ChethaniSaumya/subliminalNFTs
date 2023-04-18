308d14ee5f678817455181f68b9479894578634e
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2023-04-18 14:30:14 +0530

    Add files via upload

---
+++ precache-manifest.198797d199ea0746321ec6ee1d21aa95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c988a3bb055f8f4409e83367c5f7d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fdbd9a586689d13c15",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "e967880de7a031972af3",
+    "url": "/static/css/main.e8af04cd.chunk.css"
+  },
+  {
+    "revision": "04fdbd9a586689d13c15",
+    "url": "/static/js/2.eda6327e.chunk.js"
+  },
+  {
+    "revision": "64a3501a343d8f0c606708f692166255",
+    "url": "/static/js/2.eda6327e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed68316abdc20a421be",
+    "url": "/static/js/3.ffd6f6a6.chunk.js"
+  },
+  {
+    "revision": "e967880de7a031972af3",
+    "url": "/static/js/main.606c3188.chunk.js"
+  },
+  {
+    "revision": "0eaeb3acb40eeeb9b1ef",
+    "url": "/static/js/runtime-main.e42e18ef.js"
+  },
+  {
+    "revision": "8e22a94fffcbc19117a74b6a7b11d100",
+    "url": "/static/media/bg.8e22a94f.jpg"
+  },
+  {
+    "revision": "6c336d632af030ab5fa8d1713d5f026c",
+    "url": "/static/media/nft.6c336d63.mp4"
+  },
+  {
+    "revision": "089a47ef33ce0c5f5a29da45ec30efdd",
+    "url": "/static/media/twitter.089a47ef.png"
+  }
+]);
